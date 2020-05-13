829f0dba66e35e2077adf4ba742684d3fd35d3a0
EC2 Default User <ec2-user@ip-172-31-85-84.ec2.internal>
2020-05-13 04:49:49 +0000

    Added clarification

--- spam_ham_lambda.py
+++ spam_ham_lambda.py
@@ -9,6 +9,9 @@ import sys
 sys.path.insert(1, '/opt')
 import numpy as np
 
+"""
+The following encoding functions were taken from the AWS SageMaker spam-ham tutorial.
+"""
 if sys.version_info < (3,):
     maketrans = str.maketrans
 else:
@@ -30,17 +33,7 @@ def one_hot_encode(messages, vocabulary_length):
 def text_to_word_sequence(text,
                           filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
                           lower=True, split=" "):
-    """Converts a text to a sequence of words (or tokens).
-    # Arguments
-        text: Input text (string).
-        filters: list (or concatenation) of characters to filter out, such as
-            punctuation. Default: `!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n`,
-            includes basic punctuation, tabs, and newlines.
-        lower: boolean. Whether to convert the input to lowercase.
-        split: str. Separator for word splitting.
-    # Returns
-        A list of words (or tokens).
-    """
+
     if lower:
         text = text.lower()
 
@@ -55,21 +48,7 @@ def one_hot(text, n,
             filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
             lower=True,
             split=' '):
-    """One-hot encodes a text into a list of word indexes of size n.
-    This is a wrapper to the `hashing_trick` function using `hash` as the
-    hashing function; unicity of word to index mapping non-guaranteed.
-    # Arguments
-        text: Input text (string).
-        n: int. Size of vocabulary.
-        filters: list (or concatenation) of characters to filter out, such as
-            punctuation. Default: `!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n`,
-            includes basic punctuation, tabs, and newlines.
-        lower: boolean. Whether to set the text to lowercase.
-        split: str. Separator for word splitting.
-    # Returns
-        List of integers in [1, n]. Each integer encodes a word
-        (unicity non-guaranteed).
-    """
+
     return hashing_trick(text, n,
                          hash_function='md5',
                          filters=filters,
@@ -82,30 +61,7 @@ def hashing_trick(text, n,
                   filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
                   lower=True,
                   split=' '):
-    """Converts a text to a sequence of indexes in a fixed-size hashing space.
-    # Arguments
-        text: Input text (string).
-        n: Dimension of the hashing space.
-        hash_function: defaults to python `hash` function, can be 'md5' or
-            any function that takes in input a string and returns a int.
-            Note that 'hash' is not a stable hashing function, so
-            it is not consistent across different runs, while 'md5'
-            is a stable hashing function.
-        filters: list (or concatenation) of characters to filter out, such as
-            punctuation. Default: `!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n`,
-            includes basic punctuation, tabs, and newlines.
-        lower: boolean. Whether to set the text to lowercase.
-        split: str. Separator for word splitting.
-    # Returns
-        A list of integer word indices (unicity non-guaranteed).
-    `0` is a reserved index that won't be assigned to any word.
-    Two or more words may be assigned to the same index, due to possible
-    collisions by the hashing function.
-    The [probability](
-        https://en.wikipedia.org/wiki/Birthday_problem#Probability_table)
-    of a collision is in relation to the dimension of the hashing space and
-    the number of distinct objects.
-    """
+                      
     if hash_function is None:
         hash_function = hash
     elif hash_function == 'md5':
